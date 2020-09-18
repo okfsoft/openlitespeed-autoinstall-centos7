35122e512e2800a7dd4b056afa135ea486c817af
OKFSoft <46241578+okfsoft@users.noreply.github.com>
2020-09-19 03:35:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,27 +2,21 @@
 
 #CONFIG
 PASSRESULTS='NULL'
-GREEN='\033[0;32m'
-SET='\033[0m'
-#
-RAW_GIT=--header 'Authorization: token cfba07f91d8081d7f14189f4512348333c534e89' https://raw.githubusercontent.com/okfsoft/openlitespeed-autoinstall-centos/master
-LSWSDIR=/usr/local/lsws
+RAW_GIT=https://raw.githubusercontent.com/okfsoft/openlitespeed-autoinstall-centos7/master
+OLS_DIR=/usr/local/lsws
 
 #Random Password Generator
 function GetRandomPassword {
-    dd if=/dev/urandom bs=8 count=1 of=/tmp/randpasswdtmpfile >/dev/null 2>&1
-    PASSRESULTS=`cat /tmp/randpasswdtmpfile`
-    rm /tmp/randpasswdtmpfile
+    dd if=/dev/urandom bs=8 count=1 of=/tmp/randompassword >/dev/null 2>&1
+    PASSRESULTS=`cat /tmp/randompassword`
+    rm /tmp/randompassword
     local DATE=`date`
     PASSRESULTS=`echo "$PASSRESULTS$RANDOM$DATE" |  md5sum | base64 | head -c 32`
 }
 GetRandomPassword
-ROOTSQLPWD=$PASSRESULTS
-PMABLOWFISH=$PASSRESULTS
+PWD_SQL=$PASSRESULTS
+PWD_PMA=$PASSRESULTS
 #
 
-# Update System
-#yum -y install epel-release
-#yum -y install wget certbot openssl
-wget -O /etc/yum.repos.d/MariaDB.repo https://raw.githubusercontent.com/okfsoft/openlitespeed-autoinstall-centos/master/repository/MariaDB.repo?token=ALAZOKSRP3YNT6E473CIHVK7MUE2Y
-yum -y update
+read -e -p "PHP type to be installed [56/70/71/72/73/N] : " phpversion
+read -e -p "Install MariaDB 10.3 [y/N] : " mariadb
