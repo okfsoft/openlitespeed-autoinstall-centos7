3ead2950bdb82f84f7e21847bb202c0c581c3979
OKFSoft <46241578+okfsoft@users.noreply.github.com>
2020-09-19 04:07:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 #CONFIG
-GREEN='\033[0;32m'
-SET='\033[0m'
 PASSRESULTS='NULL'
 RAW_GIT=https://raw.githubusercontent.com/okfsoft/openlitespeed-autoinstall-centos7/master
 OLS_DIR=/usr/local/lsws
@@ -20,7 +18,7 @@ PWD_SQL=$PASSRESULTS
 PWD_PMA=$PASSRESULTS
 #
 clear
-echo -e "${GREEN}
+echo -e "
 	 OOOOOOOOOOO   KKK   KKK  FFFFFFFF  LLL             AAAAA        SSSSSSSSSSS  HHH     HHH 
 	OOO       OOO  KKK  KKK   FFFFFFFF  LLL            AAA AAA       SSSSSSSSSSS  HHH     HHH
 	OOO       OOO  KKK KKK    FFF       LLL           AAA   AAA      SSS          HHH     HHH
@@ -28,8 +26,7 @@ echo -e "${GREEN}
 	OOO       OOO  KKKKK      FFFFFFFF  LLL         AAAAAAAAAAAAA    SSSSSSSSSSS  HHHHHHHHHHH
 	OOO       OOO  KKK KKK    FFF       LLL        AAAAAAAAAAAAAAA   SSS          HHH     HHH
 	OOO       OOO  KKK  KKK   FFF       LLLLLLLL  AAA           AAA  SSSSSSSSSSS  HHH     HHH
-	 OOOOOOOOOOO   KKK   KKK  FFF       LLLLLLLL AAA             AAA SSSSSSSSSSS  HHH     HHH
-${SET}"
+	 OOOOOOOOOOO   KKK   KKK  FFF       LLLLLLLL AAA             AAA SSSSSSSSSSS  HHH     HHH"
 read -e ""
 read -e "Open Lite Speed Auto Installer - https://www.okflash.net"
 read -e ""
